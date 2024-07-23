1199c2e38c1746fc5c483d75589a1f35686d0c23
cys4585 <dev.youngsu@gmail.com>
2024-07-23 19:26:27 +0900

    ci test

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -10,6 +10,7 @@ interface ButtonProps {
 
 export default function Button(props: ButtonProps) {
   const { shape, onClick, disabled, children } = props;
+  
   return (
     <button css={shapes(shape, disabled)} onClick={onClick} disabled={disabled}>
       {children}
